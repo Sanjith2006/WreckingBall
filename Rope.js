97890eb3332c3ef1b17ca3a5543b79524915c70d
Sanjith2006 <71498115+Sanjith2006@users.noreply.github.com>
2020-11-17 15:54:03 +0530

    Add files via upload

---
+++ Rope.js
@@ -0,0 +1,26 @@
+class Rope{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length:350
+        }
+        this.pointB = pointB
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+    display(){
+       
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB;
+            push();
+            stroke(48,22,8);
+            strokeWeight(4);
+            line(pointA.x,pointA.y,pointB.x,pointB.y);
+            pop();
+
+
+    }
+    
+}
